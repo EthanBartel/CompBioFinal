61d373eba7eb46edbdc81c0fd7420e4bb14ee2d3
aohlm <aohlme1@lsu.edu>
2018-11-12 19:20:17 -0800

    Pseudocode for class Infection

---
+++ classInfectionPseudocods.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/ python
+
+class Infection
+
+#This will be our base class for different methods of infection
+#It will include viruses, bacteria, parasites
+#Each type of infection will have it's own transmission rate
+#It will include their interactions with the environment (mode of transmission, drug interaction, etc.)
+#Viruses will be the most virulent, then bacteria, then parasites
