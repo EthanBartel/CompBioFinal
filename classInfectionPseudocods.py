fc2a12a5c6ec78f5765de5ef526efc24587cd1b0
Ethan_Bartel <ebarte1@lsu.edu>
2018-11-19 20:55:29 -0600

    Update Class Files

--- classInfectionPseudocods.py
+++ classInfectionPseudocods.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env/ python
 
-class Infection
+class infection
+	"""Base Class for Infections"""
+	def __init__(self,kind="",transmission=0,resistance=0,virulence=0)
+	self.kind = kind
+	self.transmission = transmission
+	self.resistance = resistance
+	self.virulence = virulence
+
+Virus=infection(name="Virus",transmission=0.3,resistance=0.1,virulence=0.3)
+Bacteria=infection(name="Bacteria",transmission=0.2,resistance=0.2,virulence=0.2)
+Parasite=infection(name="Parasite",transmission=0.1,resistance=0.3,virulence=0.1)
 
 #This will be our base class for different methods of infection
 #It will include viruses, bacteria, parasites
