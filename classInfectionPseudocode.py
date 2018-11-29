eb9cae44ad214a51a3fb9298789a400103dbea02
Ethan Bartel <ethanbartel@ethans-macbook-pro.local>
2018-11-29 13:48:26 -0600

    Update

--- classInfectionPseudocode.py
+++ classInfectionPseudocode.py
@@ -7,62 +7,56 @@ class infection:
         self.transmission = transmission
         self.resistance = resistance
         self.virulence = virulence
-    def add_trans(self,trans):
+    def add_trans(self,trans): #function for transmission
         self.transmission.append(trans)
         self.transmission.append(sum(self.transmission))
         self.transmission.pop(0)
         self.transmission.pop(0)
-    def add_resist(self,resist):
+    def add_resist(self,resist): #function for resistance
         self.resistance.append(resist)
         self.resistance.append(sum(self.resistance))
         self.resistance.pop(0)
         self.resistance.pop(0)
-    def add_vir(self,vir):
+    def add_vir(self,vir): #function for virulence
         self.virulence.append(vir)
         self.virulence.append(sum(self.virulence))
         self.virulence.pop(0)
         self.virulence.pop(0)
 
-def air(self):
+#functions for adding factor choices
+def direct(self):
     for list in range(1):
         self.add_resist(0.04)
         self.add_trans(0.15)
         self.add_vir(0.08)
-def water(self):
+def indirect(self):
     for list in range(1):
         Virus.add_vir(0.09)
         Virus.add_trans(0.12)
         Virus.add_resist(0.06)
-def rat(self):
+def vectors(self):
     for list in range(1):
         Virus.add_vir(0.13)
         Virus.add_trans(0.08)
         Virus.add_resist(0.05)
-def bug(self):
-    for list in range(1):
-        Virus.add_vir(0.11)
-        Virus.add_trans(0.05)
-        Virus.add_resist(0.09)
 
+#setting base infections
 Virus=infection(kind="Virus",transmission=[0.3],resistance=[0.1],virulence=[0.3])
 Bacteria=infection(kind="Bacteria",transmission=[0.2],resistance=[0.2],virulence=[0.2])
 Parasite=infection(kind="Parasite",transmission=[0.1],resistance=[0.3],virulence=[0.1])
 
-print("Please select your transmission vector: \n A) Airborne - allows virus to spread though air. \n B) Waterborne - spread through water>can infect water facilities, requires moisture. \n C) Rodent - rats, mice, squirrels, and other rodents. \n D) Insect - mosquitos and other blood-sucking insects")
-print("Please select A/B/C/D:\n ")
-vector=input()
-if vector=='A':
+    """USER INTRODUCTION HERE"""
+
+#Statements for Factor Choice
+if factor=='A':
     for list in range(1):
-        air(Virus)
-if vector=='B':
+        direct(Virus)
+if factor=='B':
     for list in range(1):
-        water(Virus)
-if vector=='C':
+        indirect(Virus)
+if factor=='C':
     for list in range(1):
-        rat(Virus)
-if vector=='D':
-    for list in range(1):
-        bug(Virus)
+        vectors(Virus)
 #This will be our base class for different methods of infection
 #It will include with inherit resistance rate
 
